fb2a064631f6f0a28680ac0bfe28876aadb8a2c4
Walter Schikowski <walter.schikowski@rwth-aachen.de>
2018-06-05 17:49:27 +0200

    gower implementation for two data points untested, variable ranges need to be hardcoded

---
+++ Programmierkram/python/gower_implementation/gower_custom.py
@@ -0,0 +1,26 @@
+# Origin | Destination | Reason | Mean of Transportation | Average Time | Time Interval | Duration | Distance | Age | Gender
+o1 = [
+
+categorical = [True, True, True, True, False, True, False, False, False, True]
+
+def distance (X, Y, categorical):
+    # Check array sizes
+    
+    if (len(x) != len(Y)):
+           print("Array sizes do not Match!")
+           return;
+    
+    # Algorithm
+    
+    s = [0 for i in range(len(X))]
+    
+    ranges = [0, 0, 0, 0, 0, 0, , , , 0]
+       
+    for i in range(len(X)):
+        if (categorical[i] == True):
+            s[i] = 1 if (X[i] == Y[i]) else 0
+        else:
+            s[i] = 1 - abs(X[i] - Y[i]) / ranges[i]
+            
+    return sum(s)/len(X) 
+    
